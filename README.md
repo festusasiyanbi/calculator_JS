534c19e6bec6463db5841a2f8f7ccb47a7290095
Festus <Festusasiyanbi80@gmail.com>
2023-03-07 16:24:54 +0100

    My calculator project using JavaScript

--- README.md
+++ README.md
@@ -23,6 +23,8 @@ to discuss what you would like to change.
 
 [MIT](https://choosealicense.com/licenses/mit/)
 
+Please follow my profile, give me a star and share this project with friends and relatives that might need to see. Thank you! 😊
+
 <h3 align="left">Connect with me:</h3>
 <p align="left">
 <a href="https://linkedin.com/in/festusasiyanbi" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="festusasiyanbi" height="30" width="40" /></a>
--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-const keys = document.querySelectorAll('.key')
-const display_input = document.querySelector('.display .input')
-const display_output = document.querySelector('.display .output')
+const keys = document.querySelectorAll('.key') // This selects each key present in the 'keys' divisions
+const display_input = document.querySelector('.display .input')   //This selects the input content
+const display_output = document.querySelector('.display .output')  //This selects the output content
 
-let input = ''
-for (let key of keys) {
-    const value = key.dataset.key
-    key.addEventListener('click', () => {
-        if (value == 'clear') {
+let input = ''   //This assigns an empty string value to my input
+for (let key of keys) {   //looping through each key in the 'keys' division
+    const value = key.dataset.key  //Stores the value of each key
+    key.addEventListener('click', () => {   //Enables each key to listen for click
+            /* If 'clear' key is clicked, we do not want to display anything on the screen so we set all the contents
+            to be displayed on the wenpage to an empty string */
+            if (value == 'clear') {
             input = ''
             display_input.innerHTML = ''
             display_output.innerHTML = ''
         } else if (value == "backspace") {
-            input = input.slice(0, -1)
+            input = input.slice(0, -1) // This deletes the last index element when the backspace is clicked
             display_input.innerHTML = CleanInput(input)
         } else if (value == '=') {
-            let result = eval(PrepareInput(input))
+            let result = eval(PrepareInput(input))   //Eval() which stands for evaluation just as the names implies, evaluates the value of whatever we input
             display_output.innerHTML = CleanOutput(result)
-        } else if (value == 'brackets') {
+            /* To check and confirm which bracket to apply and when. Bracket doesn't exist when the value is -1 and it exists when it doesn't not equal -1 */
+        } else if (value == 'brackets') {   
             if (
                 input.indexOf('(') == -1 ||
                 input.indexOf('(') != -1 &&
@@ -34,7 +37,7 @@ for (let key of keys) {
                 input += ')'
             } display_input.innerHTML = CleanInput(input)
         } else {
-            if (ValidateInput(value)) {
+            if (ValidateInput(value)) {   //This displays on the screen the value of whichever key we click after all the conditions are checked
                 input += value
                 display_input.innerHTML = CleanInput(input)
             }
@@ -42,9 +45,10 @@ for (let key of keys) {
     })
 }
 
+/*The CleanInput() function changes the operators to be displayed on the screen from the hard coded ones in HTMl to the assignmed ones below */
 const CleanInput = (input) => {
-    let input_array = input.split('')
-    for (let i = 0; i < input_array.length; i++) {
+    let input_array = input.split('') // This turns all the keys into an array
+    for (let i = 0; i < input_array.length; i++) {  // I'm iterating through the array. The rest are conditions to check which key is passed in
         if (input_array[i] == '*') {
             input_array[i] = `<span class="operator">x</span>`
         } else if (input_array[i] == '/') {
@@ -60,8 +64,12 @@ const CleanInput = (input) => {
         } else if (input_array[i] == ')') {
             input_array[i] = `<span class="brackets">)</span>`
         } 
-    }   return input_array.join('')
+    }   return input_array.join('')   // joining the array back into a string
 }
+
+/* What CleanOutput() function does is that it checks whenever the output is beyond 3digits, then it seperates the last 3 elements 
+in the string with a comma. For eg. without the CleaOutput(), if we evaluate input to be 1 million, the computer would display it as 1000000
+but with the CleaOutput(), it displays as 1,000,000 and thats what the line of code in 79 does */
 
 const CleanOutput = (output) => {
     let output_string = output.toString()
@@ -77,7 +85,9 @@ const CleanOutput = (output) => {
         output_array.push(decimal)
     } return output_array.join("")
 }
-
+/* The ValidateInput() checks if we have an operator as the last input, then I don't want user to be able add more operators.
+It wouldn't make sense if user enters for example, 8+++. thus the ValidateInput checks if we already have 8+, then the rest of the '++' 
+would be ignored and return no value */
 const ValidateInput = (value) => {
     let operators = ['*', '+', '-', '/', '%', '']
     let last_input = input.slice(-1)
@@ -93,6 +103,7 @@ const ValidateInput = (value) => {
     } return true
 }
 
+/* This evaluates the percentage value when being used */
 const PrepareInput = (input) => {
     let input_array = input.split('')
     for (i = 0; i < input_array.length; i++) {
